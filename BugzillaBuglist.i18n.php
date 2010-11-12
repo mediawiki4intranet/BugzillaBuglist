2dc6fd980c8c6535c5ee279de99028a06e11fa5f
Vitaliy Filippov <vitalif@yourcmc.ru>
2010-11-12 17:55:33 +0000

    Bug 71261 - Extension to show simple buglists on wiki pages

---
+++ BugzillaBuglist.i18n.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+    'buglist-no-url' => 'No Bugzilla URL set in the config! Please see BugzillaBuglist extension source for help.',
+    'buglist-invalid-username' => 'Either username alias $1 is not configured for BugzillaBuglist, or it is configured incorrectly.',
+    'buglist-login-incorrect' => 'Invalid Bugzilla login/password for account $1.',
+    'buglist-http-error' => 'BugzillaBuglist: HTTP Error $1.'
+);
+
+$messages['ru'] = array(
+    'buglist-no-url' => 'URL Bugzilla-сервера не задан в конфигурации! Посмотрите исходник расширения BugzillaBuglist для справки.',
+    'buglist-invalid-username' => 'Учётная запись $1 не задана в конфигурации или задана некорректно для расширения BugzillaBuglist.',
+    'buglist-login-incorrect' => 'Неверный логин и/или пароль учётной записи Bugzilla $1.',
+    'buglist-http-error' => 'BugzillaBuglist: Ошибка HTTP $1.'
+);
---
+++ BugzillaBuglist.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Add a <buglist user="" query="" /> tag for inserting
+ * Bugzilla bug lists into wiki pages.
+ *
+ * Configuration:
+ * $egBugzillaBuglistUsers = array(
+ *   'alias' => array('login' => BUGZILLA LOGIN, 'password' => BUGZILLA PASSWORD),
+ * );
+ * $egBugzillaBuglistCacheTime = 10; // seconds
+ * $egBugzillaBuglistUrl = 'http://bugzilla.office.local/';
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Vitaliy Filippov <vitalif@mail.ru>
+ * @copyright (c) 2010 Vitaliy Filippov
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+if (!defined('MEDIAWIKI'))
+    die("This file is an extension to the MediaWiki software and cannot be used standalone");
+
+$wgExtensionFunctions[] = 'efBugzillaBuglist';
+$wgExtensionMessagesFiles['BugzillaBuglist'] = dirname(__FILE__).'/BugzillaBuglist.i18n.php';
+$wgExtensionCredits['parserhook'][] = array(
+    'name'    => 'Bugzilla Buglist',
+    'author'  => 'Vitaliy Filippov',
+    'version' => '0.9a',
+);
+
+if (!$egBugzillaBuglistUsers)
+    $egBugzillaBuglistUsers = array();
+if (!$egBugzillaBuglistCacheTime)
+    $egBugzillaBuglistCacheTime = 10;
+if (!$egBugzillaBuglistUrl)
+    $egBugzillaBuglistUrl = '';
+
+function efBugzillaBuglist()
+{
+    global $wgParser;
+    $wgParser->setHook('buglist', 'efRenderBugzillaBuglist');
+}
+
+/* Parser function, returns wiki-text */
+function efRenderBugzillaBuglist($content, $args, $parser)
+{
+    global $egBugzillaBuglistUsers, $egBugzillaBuglistUrl, $egBugzillaBuglistCacheTime;
+    $username = $args['user'];
+    $query = $args['query'];
+    wfLoadExtensionMessages('BugzillaBuglist');
+    if (!$egBugzillaBuglistUrl)
+        return wfMsgNoTrans('buglist-no-url');
+    if (!$egBugzillaBuglistUsers[$username] ||
+        !$egBugzillaBuglistUsers[$username]['login'] ||
+        !$egBugzillaBuglistUsers[$username]['password'])
+        return wfMsgNoTrans('buglist-invalid-username', $username, $query);
+    $url = $egBugzillaBuglistUrl;
+    if (substr($url, -1) != '/')
+        $url .= '/';
+    $cache = wfGetCache(CACHE_ANYTHING);
+    $contentkey = wfMemcKey('bugzilla_buglist', $username, $query);
+    if (!($html = $cache->get($contentkey)))
+    {
+        $authkey = wfMemcKey('bugzilla_logincookie', $username);
+        if (!preg_match('/\d/', $cookie = $cache->get($authkey)))
+        {
+            /* Log in to Bugzilla */
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url . 'index.cgi');
+            curl_setopt($curl, CURLOPT_HEADER, 1);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array(
+                'GoAheadAndLogIn'   => 1,
+                'Bugzilla_login'    => $egBugzillaBuglistUsers[$username]['login'],
+                'Bugzilla_password' => $egBugzillaBuglistUsers[$username]['password'],
+            )));
+            $head = curl_exec($curl);
+            if (($code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) != 200)
+            {
+                $cache->delete($authkey);
+                return wfMsgNoTrans('buglist-http-error', $code);
+            }
+            else
+            {
+                $head = substr($head, 0, strpos($head, "\n\n"));
+                preg_match('#Set-Cookie:\s*Bugzilla_login=(\d+|X)#is', $head, $m);
+                $l = $m[1];
+                preg_match('#Set-Cookie:\s*Bugzilla_logincookie=([A-Za-z0-9]+)#is', $head, $m);
+                $c = $m[1];
+                if (!$l || !$c || $l == 'X' || $c == 'X')
+                {
+                    $cache->delete($authkey);
+                    return wfMsgNoTrans('buglist-login-incorrect', $egBugzillaBuglistUsers[$username]['login']);
+                }
+                $cookie = "Cookie: Bugzilla_login=$l; Bugzilla_logincookie=$c";
+                /* Remember the cookie */
+                $cache->set($authkey, $cookie, 86400);
+            }
+        }
+        /* Fetch page content */
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, $url . 'buglist.cgi?format=simple&cmdtype=runnamed&namedcmd='.urlencode($query));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array($cookie));
+        $html = curl_exec($curl);
+        if (($code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) != 200)
+            return wfMsgNoTrans('buglist-http-error', $code);
+        /* Clean HTML code */
+        // extract body
+        preg_match('#<body[^<>]*>(.*)</body#is', $html, $m);
+        $html = $m[1];
+        // remove <thead> and <tbody> for js-sortable tables to work
+        $html = preg_replace('#</?t(head|body)[^<>]*>#is', '', $html);
+        // add unsortable class for time summary line
+        $html = str_replace('bz_time_summary_line', 'bz_time_summary_line unsortable', $html);
+        // remove buglist links (resort etc)
+        $html = preg_replace('#<a[^<>]*href=["\'][^"\']*buglist.cgi[^"\']*["\'][^<>]*>(.*?)</a\s*>#is', '\1', $html);
+        // make other links absolute
+        $html = preg_replace('#<a\s+([^<>]*href=["\'])(?!https?://)#is', '<a target="_blank" \1'.$url, $html);
+        $cache->set($contentkey, $html, $egBugzillaBuglistCacheTime);
+    }
+    /* Protect the HTML code from being escaped */
+    $marker = $parser->mUniqPrefix."-buglist-" . sprintf('%08X', $parser->mMarkerIndex++) . Parser::MARKER_SUFFIX;
+    $parser->mStripState->nowiki->setPair($marker, $html);
+    $parser->mOutput->addHeadItem("<link rel='stylesheet' type='text/css' href='$url/skins/standard/buglist.css' />");
+    return $marker;
+}
